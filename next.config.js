c72d66628b3fe5926bb7502929fbbec1580bbaf8
kenny miller <116318994+eyekayem@users.noreply.github.com>
2025-01-31 16:32:05 -0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+const { withNextVideo } = require("next-video/process");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}; // Keep this empty or add any custom Next.js config here.
+
+module.exports = withNextVideo(nextConfig);
