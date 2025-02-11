b98ac46fb5be2bd0a4aa9e4539a5201a9c1d3f7b
kenny miller <116318994+eyekayem@users.noreply.github.com>
2025-02-10 16:00:35 -0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const { withNextVideo } = require("next-video/process");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {}; // Keep this empty or add any custom Next.js config here.
-
-module.exports = withNextVideo(nextConfig);
